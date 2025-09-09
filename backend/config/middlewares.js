e0c923e6fdf2712e5d61954548c77bb0d75301ba
Arthur Sathler <asathler@vertigo.com.br>
2025-09-08 23:20:51 -0300

    fix(pokeloja): fix public url for admin

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,7 +1,20 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:', 'https://kairyuutcg.com.br', 'http://kairyuutcg.com.br'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io'],
+          'media-src': ["'self'", 'data:', 'blob:'],
+          'frame-src': ["'self'"],
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
